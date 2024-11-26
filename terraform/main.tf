686a44bd0ea66aa4bb2b6af43f1e7db8f42668e2
crisxyrus@live.com <crisxyrus>
2024-11-27 01:13:26 +0800

    adding updated main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,4 +12,7 @@ resource "aws_instance" "docker-deployment" {
   ami = var.ami # Replace with a valid AMI ID 
   instance_type = var.instance_type
   key_name = var.key_name # Reference the key pair
+  tags = {
+    Name = "VMDEV01"
+  }
 }
